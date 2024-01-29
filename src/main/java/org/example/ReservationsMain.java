5c0fef31c4d0471358897a7ae7228f1a7ecd6ef9
Nkateko21-web <mbalikn3@gmail.com>
2024-01-29 10:19:10 +0200

    Project completed

--- src/main/java/org/example/ReservationsMain.java
+++ src/main/java/org/example/ReservationsMain.java
@@ -1,24 +1,24 @@
-package org.example;
-
-import java.sql.Connection;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class ReservationsMain {
-
-    static final String JDBC_URL = "jdbc:mysql://localhost/reservation_system";
-    static final String USER = "root@localhost";
-    static final String PASSWORD = "password";
-
-    public static void main(String[] args) {
-        try(Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD)) {
-            ReservationSystem reservationSystem = new ReservationSystem();
-
-            reservationSystem.makeReservation(connection);
-            reservationSystem.cancelReservation(connection);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//package org.example;
+//
+//import java.sql.Connection;
+//
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class ReservationsMain {
+//
+//    static final String JDBC_URL = "jdbc:mysql://localhost/reservation_system";
+//    static final String USER = "root@localhost";
+//    static final String PASSWORD = "password";
+//
+//    public static void main(String[] args) {
+//        try(Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD)) {
+//            ReservationSystem reservationSystem = new ReservationSystem();
+//
+//            reservationSystem.makeReservation(connection);
+//            reservationSystem.cancelReservation(connection);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
